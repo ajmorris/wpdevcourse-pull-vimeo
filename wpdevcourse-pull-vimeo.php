a59c332d715b2e67556114b43a3bc1ed50825980
AJ Morris <aj@ajmorris.me>
2019-06-27 11:20:40 -0400

    Create wpdevcourse-pull-vimeo.php

---
+++ wpdevcourse-pull-vimeo.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Pull in Vimeo Videos
+Plugin URI: https://ajmorris.me
+Description: Pulls in videos from my vimeo channel
+Version: 0.1.0
+Author: iThemes
+Author URI: https://ithemes.com
+Text Domain: ithemes-vimeo
+*/
